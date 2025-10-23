f319b940b86e5f9bfca06739477abaaa5cd0af7e
sabinurr <zhansulu.mukhamedsapa@jihc.edu.kz>
2025-10-23 09:09:10 +0500

    описание изменений

--- pages/index.vue
+++ pages/index.vue
@@ -8,8 +8,14 @@ import { useRouter } from "#app";
 const router = useRouter();
 
 onMounted(() => {
+<<<<<<< HEAD
   router.replace("/admin/category");
 
   router.replace("/client");
 });
 </script>
+=======
+  router.replace('/admin/category')
+})
+</script>
+>>>>>>> 5c3ccdd (описание изменений)
